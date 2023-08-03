6c1c224cb70d86698350cee4c7f861cd014896d0
Luke Kennedy <73356627+lkenn012@users.noreply.github.com>
2023-08-03 13:35:14 -0400

    Update get_DeepGOAnnots.py
    Adjusted code and formatting for clarity

--- deepGOWeb/get_DeepGOAnnots.py
+++ deepGOWeb/get_DeepGOAnnots.py
@@ -8,11 +8,6 @@ import numpy as np
 
 import get_FASTAseqs as ensgDB
 
-# load ensembl IDs
-path = r"C:\Users\User\OneDrive\Desktop\School and Work\Programming\MastersPython\mGSH manuscript code"
-ensg_ids = pd.ExcelFile(rf"{path}\data\mGSH_labeledGenes_HighConAnnots.xlsx")
-
-
 # define function to call the DeepGO web server to predict a given sequence
 def get_DeepGO(FASTA, annot_threshold=0.3):
 
@@ -24,7 +19,7 @@ def get_DeepGO(FASTA, annot_threshold=0.3):
 	return result
 
 # define a function to check deepGO annotation set for a specific GO term for calculating sensitivity 
-def deepGO_sensitivity(go, annotations, go_class=None):
+def get_GOAnnot(go, annotations, go_class=None):
 
 	# given a json return from deepGO get the predicted functions
 	try:
@@ -32,7 +27,8 @@ def deepGO_sensitivity(go, annotations, go_class=None):
 	except KeyError as e:
 		if str(e) == '\'predictions\'': # get this error when no protein sequence is available for our original gene
 			return 'NA'
-
+			
+	# check the probability of the annotation of our id of interest and return it
 	if go_class:
 		for go_group in go_annots:
 			if go_group['name'] == go_class:
@@ -45,85 +41,83 @@ def deepGO_sensitivity(go, annotations, go_class=None):
 			for functions in go_group['name']['functions']:
 				if functions[1] == go:
 					return functions[2]
+					
+	# if the go term of interest is not found, return probability = 0
 	return 0
+	
+# define function to take in a list of genes for a given GO term, from our annotation file, and get corresponding FASTA sequence and deepgo annotations
+def annotate_Genes(annot_ids, n_samples=False):
 
-# define main function to get FASTA seqs and then DeepGO annotations
-def main():
-
-	# specify annotations of interest
-	annot_ids = pd.read_excel(ensg_ids, sheet_name='GSH Ensembl')
-	print(f'annot_ids:\n{annot_ids}')
-
-	# annot_ids = annot_ids.sample(n=150, random_state=42)
-	# print(f'random subset of 150 IDs:\n{annot_ids}')
-
-
+	# Optionally, if there are many genes for our GO term of interest, may want to annotate a subset due to slow speed of DeepGOWeb
+	if n_samples:
+		annot_ids = np.random.choice(annot_ids, n_samples)
 	annots = []
 	# Iterate over our list of annot_ids
-	for ensg_id in annot_ids.iloc[:,0].to_list():
+	for ensg_id in annot_ids:
 
 		time.sleep(1)
 		canonTrans_id = ensgDB.get_canonicalTranscript(ensg_id.strip()) 	# get transcript ID
 
 		# check if esng_id is found in ensembl, use corresponding canonical transcript to get fasta
 		if canonTrans_id:
-
 			fasta = ensgDB.get_proteinSeq(canonTrans_id) 	# get FASTA sequence
-
 			deepGO_annot = get_DeepGO(fasta) 	# get deepGO annotation
-
-			annots.append({'Ensembl gene ID': ensg_id, 'DeepGO annotation': deepGO_annot})
+			
+			annots.append({'Ensembl gene ID': ensg_id, 'DeepGO annotation': deepGO_annot}) 	# save annotations
 
 		else:
 			print(f' **   ID not found in ensembl: {ensg_id}')
 
-	# convert to DF and save as pkl file for analysis
-	annot_df = pd.DataFrame(annots)
-	annot_df.to_pickle(rf'{path}\outputs\gsh_deepGOAnnots.pkl')
+	# return the annotated file as a dataframe
+	return pd.DataFrame(annots)
 
-# function for checking annotations for a specific go term, when we already have an annotation file
-def main2():
+# define function for getting deepGO annotations from a list of ensembl gene IDs, and calculate deepGO annotation sensitivity for term of interest
+def deepGOAnnots_fromIDs(ensgIDs, go_interest, annot_samples=False, class_thresh = [0, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.0]):
 
-	## Specify parameters of interest
-	#
+	# Get the DeepGO annotations from our list of ids
+	annot_df = annotate_Genes(annot_ids=ensgIDs, n_samples=annot_samples)
 
-	go_interest = ('GO:0005739', "Cellular Component") 	# mitochondrial localization
-	# go_interest = ('GO:0006749', "Biological Process") 	# glutathione metabolic process
-	# go_interest = ('GO:0022857', "Molecular Function") 	# transmembrane transporter activity
-
-	f_name = 'randMito_deepGOAnnots' 	# mitochondrial term annotations
-	# f_name = 'gsh_deepGOAnnots' 	# gsh term annotations
-	# f_name = 'randTransp_deepGOAnnots' 	# transporter term annotations
-
-
-
-	# load data
-	annot_df = pd.read_pickle(rf'{path}\outputs\{f_name}.pkl')
-
+	# Now, calculate the performance of DeepGO for annotating our term of interest
 	GO_probs = []
 	for i, annot in annot_df.iterrows():
+		GO_probs.append(get_GOAnnot(go=go_interest[0], annotations=annot[1], go_class=go_interest[1])) 	# check for annotations for our go term of interest
 
-		GO_probs.append(deepGO_sensitivity(go=go_interest[0], annotations=annot[1], go_class=go_interest[1])) 	# check for gsh metabolic process go term
-
-	print(f'GO probs:\n{GO_probs}')
-
+	# add this annotation as a new field in our annotated df
 	annot_df[f'{go_interest[0]} prob.'] = GO_probs
 
 	# sensitivity calculation
-	annotated = [i for i in GO_probs if i != 'NA']
+	annotated = [i for i in GO_probs if i != 'NA'] 	# get all annotated genes
 
-	gshPred_count = len([i for i in annotated if i > 0.50])
-	print(f'GO sensitivity: {gshPred_count / len(annotated)}')
-
-	print(f'annot_df:\n{annot_df}')
-	annot_df.to_csv(rf'{path}\outputs\{f_name}_wConfidence.csv')
-	return
+	sensitivities = []
+	for thresh in class_thresh:
+		annotPred_count = len([i for i in annotated if i > thresh]) 	# count annotations above threshold
+		sensitivities.append({thresh: annotPred_count/len(annotated)})
+		
+	# return the deepGO annotations, and corresponding sensitivities for our go term of interest
+	return annot_df, sensitivities
 
 
 
 ##
-## RUN MAIN CODE
+## DEFINE AND RUN MAIN for deepGO annotation
 ##
 
-# main()
-main2()
+def main():
+	
+	## load annotated ensembl IDs for GO terms of interest as pandas DF
+	data_path = {path} 	# Define the path to the annotation file
+	ensg_ids = pd.ExcelFile(rf"{data_path}\mGSH_labeledGenes_HighConAnnots.xlsx")
+	goTerm_ids = pd.read_excel(ensg_ids, sheet_name='GSH Ensembl') 	# GSH annotated IDs
+
+	## Specify the GO terms and class of interest for quantifying DeepGOPlus annotation performance, below are those used for the reported analysis.
+	go_term = ('GO:0006749', "Biological Process") 	# glutathione metabolic process
+	# go_term = ('GO:0005739', "Cellular Component") 	# mitochondrial localization
+	# go_term = ('GO:0022857', "Molecular Function") 	# transmembrane transporter activity
+
+	annotated_df, annot_sensitivity = deepGOAnnots_fromIDs(ensgIDs=goTerm_ids.iloc[:,0].to_list(), 	# convert pandas object to array
+					    go_interest=go_term, 
+					    annot_samples=False
+					   )
+
+## RUN MAIN
+main()
