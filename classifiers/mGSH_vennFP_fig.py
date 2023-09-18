1ddae7248c9025cd3d6fab9e05082e6892d37eb6
Luke Kennedy <73356627+lkenn012@users.noreply.github.com>
2023-09-18 14:25:33 -0400

    Update mGSH_vennFP_fig.py
    Edited for clarity

--- classifiers/mGSH_vennFP_fig.py
+++ classifiers/mGSH_vennFP_fig.py
@@ -1,17 +1,21 @@
-### generate venn diagrams
+# Code for generating venn diagrams of classifier False positives (FPs)
+# Values for Venn diagrams are obtained from supplementary data
 
-# import modules
+##
+## import modules
+##
+
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn2, venn3
 
+# Create venn diagram based on MitoCarta FPs, random forest FPs and the overlapping FPs
+venn2(subsets=(6,229,615), set_labels = ('MitoCarta False Positives', 'RF False Positives'), set_colors=('rebeccapurple', 'mediumseagreen'), alpha=0.50)
+plt.tight_layout()
+plt.savefig('mitoFP_venn.png', dpi=1600)
 
-# Define sets for venn diagram
-# venn3(subsets=(6,206,615,0,0,23,0), set_labels = ('MitoCarta False Positives', 'RF False Positives', 'RF False Positives, 1+ evidence'))
-# venn2(subsets=(6,229,615), set_labels = ('MitoCarta False Positives', 'RF False Positives'), set_colors=('rebeccapurple', 'mediumseagreen'), alpha=0.50)
-# venn2(subsets=(0,206,23), set_labels = ('RF FPs, 1+ evidence', 'RF FPs, 0 evidence'), set_colors=('saddlebrown', 'mediumseagreen'), alpha=0.60)
-# plt.tight_layout()
-# plt.savefig('test FP venn diagram_RF-only.png', dpi=1600)
-
+# Repeat for transporter classifier and TrSSP
 venn2(subsets=(147,218,1477), set_labels =('TrSSP False Positives', 'RF False Positives'), set_colors=('peru', 'mediumseagreen'), alpha=0.60)
 plt.tight_layout()
-plt.savefig('TrSSP_FP_venn.png', dpi=1600)
+plt.savefig('transpFP_venn.png', dpi=1600)
+
+## NOTE: no way to adjust positioning of labels in venn diagrams, must adjust manually for publication using Biorender, Adobe Illustrator, or some other software
