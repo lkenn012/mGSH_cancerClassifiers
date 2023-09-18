e3b4281338e0619b8a0de83ec4d497cdf540ffaa
Luke Kennedy <73356627+lkenn012@users.noreply.github.com>
2023-09-18 11:50:36 -0400

    Update preprocessing_mGSH.py
    Updated for clarity
    
    Code rewritten to remove unecessary code, rewrite code and add comments for clarity

--- preprocessing_mGSH.py
+++ preprocessing_mGSH.py
@@ -1,6 +1,9 @@
-## Pre-processing data for features in mGSH model
+## Pre-processing data for features in mGSH classifier models. Cleans and imputes missing values from CCLE transcriptomics and metabolomics
 
-# import modules
+##
+## import modules
+##
+
 import pandas as pd
 import numpy as np
 
@@ -8,6 +11,10 @@ import sklearn.preprocessing as skpr
 from sklearn.impute import KNNImputer
 
 from  main_mGSH import spearmanr_pval, threshold_corr	# for generating correlations between GSH/GSSG and transcripts
+
+##
+## Define methods for data preprocessing for classifier models
+##
 
 # define a function to check given data for variables with standard deviation=0 (or ={threshold}) across samples, returns truncated DF with those rows removed
 # Note: check_axis determines which axis std dev will be calculated across, so =0 gets a row of std dev corresponding to col data
@@ -34,6 +41,7 @@ def check_stdDev(data_df, check_axis=0, threshold=0):
 	return trunc_dataDF
 
 # this function checks cols for NaNs and removes any with NaN > threshold
+# default NaN threshold is 0.30 (used in paper), also provides optional argument for a NaN str to be replaced
 def clean_nan(data_df, threshold=0.3, na_str=None):
 
 	# create copy of data DF
@@ -69,153 +77,67 @@ def impute_data(data_df):
 	return imputed_copyDF
 
 # define main function, which will clean and impute the CCLE metabolomics & transcriptomics data used in mGSH classifier code
+# Also generate correlations between gene transcriptomics data and metabolite metabolomics data to be used in classifiers ('glutathione reduced', 'glutathione oxidized', 'alpha-ketoglutarate', 'carnitine', 'glutamate')
 def main():
 
 	# load data
-	# path = r'\data'
-	path = r'C:\Users\lkenn\OneDrive\Desktop\School and Work\Programming\MastersPython\mGSH manuscript code\data'
-	# gene_df = pd.read_csv(rf'{path}\BroadInstitute-CCLE_RNAseq_rsem_genes_tpm_20180929_csv.csv', index_col=0)
-	# metab_df = pd.read_excel(rf'{path}\41591_2019_404_MOESM2_ESM.xlsx', sheet_name='1-raw data', index_col=0, usecols='A:EG') 	# raw metab data with lipids removed
+	data_path = "path" 	# !! PLACEHOLDER !! replace with the path to your dat directory
+	gene_df = pd.read_csv(rf'{data_path}\BroadInstitute-CCLE_RNAseq_rsem_genes_tpm_20180929_csv.csv', index_col=0) 	# CCLE raw transcriptomics data
+	metab_df = pd.read_excel(rf'{data_path}\41591_2019_404_MOESM2_ESM.xlsx', sheet_name='1-raw data', index_col=0) 	# CCLE raw metab data
 
-	# # process metabolomic data
-	# print(f'Original df:\n{metab_df}')
+	# process metabolomic data
 
-	# # remove CCLs, then metabs with STD dev. = 0 across samples
-	# temp_metabDF = check_stdDev(metab_df, check_axis=1, threshold=0)
-	# temp_metabDF = check_stdDev(temp_metabDF, check_axis=0, threshold=0)
+	# remove CCLs, then metabs with STD dev. = 0 across samples
+	temp_metabDF = check_stdDev(metab_df, check_axis=1, threshold=0)
+	temp_metabDF = check_stdDev(temp_metabDF, check_axis=0, threshold=0)
 
-	# # remove CCLS, then metabs with >30% NAN samples from DF
-	# temp_metabDF = clean_nan(temp_metabDF.T, threshold=0.30, na_str='NA')
-	# temp_metabDF = clean_nan(temp_metabDF.T, threshold=0.30, na_str='NA')
+	# remove CCLS, then metabs with >30% NAN samples from DF
+	temp_metabDF = clean_nan(temp_metabDF.T, threshold=0.30, na_str='NA')
+	temp_metabDF = clean_nan(temp_metabDF.T, threshold=0.30, na_str='NA')
 
-	# # Finally, impute remaining NANs using K-NN and save cleaned data
-	# cleaned_metabDF = impute_data(temp_metabDF)
-	# print(f'Final metab df, KNN imputed:\n{cleaned_metabDF}')	
+	# Finally, impute remaining NANs using K-NN and save cleaned data
+	cleaned_metabDF = impute_data(temp_metabDF)
 
-	# cleaned_metabDF.to_csv(rf'{path}\cleaned_ccle_metabolomics.csv')
+	# process transcriptomics data as with metabolomics
 
-	# generate spearman correlations for GSH, GSSG across transcripts
+	# remove CCLs, then transcripts with STD dev. = 0 across samples
+	temp_geneDF = check_stdDev(gene_df, check_axis=1, threshold=0)
+	temp_geneDF = check_stdDev(temp_geneDF, check_axis=0, threshold=0)
+
+	# remove CCLS, then transcripts with >30% NAN samples from DF
+	temp_geneDF = clean_nan(temp_geneDF.T, threshold=0.30, na_str='NA')
+	temp_geneDF = clean_nan(temp_geneDF.T, threshold=0.30, na_str='NA')
+
+	# Finally, impute remaining NANs using K-NN and save cleaned data
+	cleaned_geneDF = impute_data(temp_geneDF)
+	print(f'Final gene df, KNN imputed:\n{cleaned_geneDF}')		
+
+	cleaned_geneDF.to_csv(rf'{path}\cleaned_ccle_rnaSeq.csv') 	# optionally, save the cleaned data for future use
+	cleaned_metabDF.to_csv(rf'{path}\cleaned_ccle_metabolomics.csv')
+	
+
+	# generate spearman correlations for metabolites of interest for classsifier model features
 	# Also want to seperate transcriptomics data CCLs that are/aren't shared with metabolomics data
-	# Common CCL data will be used in model training/testing, remaing CCL data can be used as validation set
-	gene_df = pd.read_csv(rf'{path}\cleaned_ccle_rnaSeq.csv', index_col=0)
-	metab_df = pd.read_csv(rf'{path}\cleaned_ccle_metabolomics.csv', index_col=0)
 
-	print(f'DFs to be correlated:\nMetab:\n{metab_df}\ngene df:\n{gene_df}')
+	# Now want only common CCLs for correlations
+	common_CCLS = cleaned_metabDF.index.intersection(cleaned_geneDF.columns)
 
-	# Now want only common CCLS for correlations
-	common_CCLS = metab_df.index.intersection(gene_df.columns)
-	print(f'\nCommon CCLS:\n{common_CCLS}')
-
-	common_metabDF = metab_df.loc[common_CCLS]
-	common_geneDF = gene_df.loc[:,common_CCLS].copy()
-	uniq_geneDF = gene_df.drop(common_CCLS, axis=1)
-	print(f'Common CCL data:\nMetab:\n{common_metabDF}\nGene:\n{common_geneDF}')
+	common_metabDF = cleaned_metabDF.loc[common_CCLS]
+	common_geneDF = cleaned_geneDF.loc[:,common_CCLS].copy()
+	uniq_geneDF = cleaned_geneDF.drop(common_CCLS, axis=1)
 
 	# Now generate correlations between the data, and then threshold based on p-vals & set to absollute
-	metab_cols = common_metabDF.loc[:, ['alpha-ketoglutarate', 'carnitine', 'glutamate']]
-	print(f'metab cols:\n{metab_cols}')
+	metab_cols = common_metabDF.loc[:, ['glutathione reduced', 'glutathione oxidized', 'alpha-ketoglutarate', 'carnitine', 'glutamate']]
 
+	# Now we can calculate spearman correlations for each column (metabolite) of data in metabolomics data, then threshold based on p-values
 	metab_corrs = metab_cols.apply(lambda x: common_geneDF.corrwith(x, axis=1, method='spearman'))
-	print(f'Corrwith apply:\n{metab_corrs}')
 	corr_pvals = metab_cols.apply(lambda x: common_geneDF.corrwith(x, axis=1, method=spearmanr_pval))
 
 	threshMetab_corrs = threshold_corr(metab_corrs, corr_pvals)
 	finalMetab_corrs = threshMetab_corrs.abs()
-	print(f'Final metab corrs:\n{finalMetab_corrs}')
 
-	finalMetab_corrs.to_csv(rf'{path}\nonGSH_metab_spearman.csv')
-	# common_geneDF.to_csv(rf'{path}\cleaned_common_ccleRNAseq.csv')
-	# uniq_geneDF.to_csv(rf'{path}\cleaned_unique_ccleRNAseq.csv')
+	finalMetab_corrs.to_csv(rf'{data_path}\ccle_metab_spearman.csv') 	# Save outputs for use in models
 
 
-	# # process transcriptomics data as with metabolomics
-	# print(f'Original df:\n{gene_df}')
-
-	# # remove CCLs, then transcripts with STD dev. = 0 across samples
-	# temp_geneDF = check_stdDev(gene_df, check_axis=1, threshold=0)
-	# temp_geneDF = check_stdDev(temp_geneDF, check_axis=0, threshold=0)
-
-	# # remove CCLS, then transcripts with >30% NAN samples from DF
-	# temp_geneDF = clean_nan(temp_geneDF.T, threshold=0.30, na_str='NA')
-	# temp_geneDF = clean_nan(temp_geneDF.T, threshold=0.30, na_str='NA')
-
-	# # Finally, impute remaining NANs using K-NN and save cleaned data
-	# cleaned_geneDF = impute_data(temp_geneDF)
-	# print(f'Final gene df, KNN imputed:\n{cleaned_geneDF}')		
-
-	# cleaned_geneDF.to_csv(rf'{path}\cleaned_ccle_rnaSeq.csv')
-
+# Run code for preprocessing data
 main()
-
-
-
-### !! TO DO !!
-###	normalize data? Fisher transform after correlation generated, so no norm needed.
-
-####																								  ####
-##	Source code for above, taken from preporcessing.py and CCLE_metab_SLC_NBclassifier_GET_FEATURES.py 	##
-####																								  ####
-
-# ## Need to examine proteomics data and manage NaN values
-# ## Check amount of NaNs per protein, and NaNs per CCL. Consider removing these pns/ccls from data if NaNs >25%
-# def nan_and_imputeData(prot_data):
-
-# 	print(f'Initial prot_data, including NaN row/cols:\n{prot_data}\nnew DF shape: {prot_data.shape}')
-
-# 	# First, remove NaN cols (CCLs) to preserve protein data
-# 	prot_cols = list(prot_data.columns)
-
-# 	ccl_NANs = [prot_data.loc[:, x].isna().sum() for x in prot_cols] 	# get na counts across cols
-# 	ccl_NANPct = [x / len(prot_data.index) for x in ccl_NANs] 		# get NaN as % of col length
-
-# 	drop_idxs = []
-# 	for idx, percent in enumerate(ccl_NANPct):
-# 		if percent >= 0.30:
-# 			drop_idxs.append(idx)
-# 	prot_data.drop(prot_data.columns[drop_idxs], axis=1, inplace=True)	# remove these col indexes from prot df
-
-# 	# repeat for rows w/ update DF
-# 	prot_rows = list(prot_data.index)
-
-# 	pn_NANs = [prot_data.iloc[x].isna().sum() for x in range(0,len(prot_rows))]	# get na counts across rows note: use iloc due to duplicate labels in rows
-
-# 	pn_NANPct = [x / len(prot_cols) for x in pn_NANs] 	# get percent of axis as nans
-
-# 	# if nan % is above threshold (e.g. 30%) drop these cols/rows from data
-# 	drop_idxs = []
-# 	for idx, percent in enumerate(pn_NANPct):
-# 		if percent >= 0.30:
-# 			drop_idxs.append(idx)
-# 	prot_data.drop(prot_data.index[drop_idxs], inplace=True)	# drop these row indexes from prot data
-
-# 	print(f'\nNew prot_data, with nan row/cols removed:\n{prot_data}\nnew DF shape: {prot_data.shape}')
-
-# 	# now that we have nan vectors removed, need to impute remaining nan values in rows
-# 	# use K-NN algorithm from sci-kit for imputing these values (better than mean imputation)
-
-# 	imputer = KNNImputer() 	# default params, n_neighbours = 5, etc.
-# 	knn_protData = imputer.fit_transform(prot_data)
-# 	imputed_protDF = pd.DataFrame(knn_protData, index=prot_data.index, columns=prot_data.columns)
-
-# 	return imputed_protDF
- 
-
-# 	#Read in the input file name as pd df
-# 	raw_df = pd.read_csv(filename, index_col=0, na_filter=False)
-# 	raw_df = raw_df.replace('NA',np.NaN) #Replace NA with np.NaN for sklearn functions
-# 	#sklearn methods return np_arrays, get df index and column to be used in output
-# 	df_indexes = list(raw_df.index.values)
-# 	df_columns = list(raw_df.columns.values)
-
-# 	#impute missing values using k-Nearest Neighbours, sklearn.KNNImputer
-# 	imputer = KNNImputer()
-# 	imputed_array = imputer.fit_transform(raw_df)
-
-# 	#log scale data then normalize using np.log10() and skpr.normalize() functions
-# 	log_array = np.log10(imputed_array)
-# 	processed_array = skpr.normalize(log_array)
-
-# 	#Create dataframe from the preprocessed data
-# 	preprocessed_df = pd.DataFrame(data=processed_array, index=df_indexes, columns=df_columns)
-
-# 	return(preprocessed_df)
